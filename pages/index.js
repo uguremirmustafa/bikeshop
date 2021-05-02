ac5759b34412efda564d580bcf98d4cd2fddb01b
uguremirmustafa <uguremirmustafa@gmail.com>
2021-05-02 15:09:43 +0300

    auth0 setup complete

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,22 @@
-export default function HomePage() {
-  return (
-    <div>
-      <h2>hello</h2>
-    </div>
-  );
+import { useUser } from '@auth0/nextjs-auth0';
+import Link from 'next/link';
+
+export default function Home() {
+  const { user, error, isLoading } = useUser();
+
+  if (isLoading) return <div>Loading...</div>;
+  if (error) return <div>{error.message}</div>;
+
+  if (user) {
+    return (
+      <div>
+        Welcome {user.name}! <a href="/api/auth/logout">Logout</a>
+        <div>
+          <Link href="/api/auth/login">login</Link>
+        </div>
+      </div>
+    );
+  }
+
+  return <a href="/api/auth/login">Login</a>;
 }
